12fbbff8efa37f754b2c2a292c9e4f6a9c95cccf
Sendoa Barciela <sendoa.barciela@docomodigital.com>
2019-01-15 15:09:11 +0100

    Add fakerjs and default data

--- src/Main/index.js
+++ src/Main/index.js
@@ -3,37 +3,58 @@ import AddItemsSection from 'components/ToDoList/AddItemsSection'
 import ItemsListSection from 'components/ToDoList/ItemsListSection'
 import MlIcon from 'MyLib/Icon'
 
+const getInitialState = () => {
+  const initialTaskNumber = window.faker.random.number({ min: 1, max: 5 })
+  const taskList = []
+
+  for (var i = 0; i < initialTaskNumber; i++) {
+    taskList.push({
+      id: i,
+      name: window.faker.lorem.sentence(),
+      status: 'not-done'
+    })
+  }
+
+  return {
+    taskList,
+    currentTask: {
+      id: null,
+      name: '',
+      status: 'not-done'
+    }
+  }
+}
+
 class Main extends Component {
   constructor () {
     super()
-    this.state = {
-      taskList: [],
-      currentTask: {
-        id: null,
-        name: '',
-        status: 'not-done'
-      }
-    }
+    this.state = getInitialState()
 
+    this.handleReset = this.handleReset.bind(this)
     this.handleItemAdd = this.handleItemAdd.bind(this)
     this.handleNotDone = this.handleNotDone.bind(this)
     this.handleDone = this.handleDone.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
   }
 
+  handleReset () {
+    this.setState(getInitialState())
+  }
+
   handleItemAdd (data) {
-    data.id = !!this.state.taskList[this.state.taskList.length - 1] ?
+    const taskId = !!this.state.taskList[this.state.taskList.length - 1] ?
       (this.state.taskList[this.state.taskList.length - 1].id + 1) :
       1
 
     const taskList = [
       ...this.state.taskList,
-      data
+      {
+        ...data,
+        id: taskId
+      }
     ]
 
-    this.setState({
-      taskList: taskList
-    })
+    this.setState({ taskList: taskList })
 
     this.setState({
       currentTask: {
@@ -45,7 +66,7 @@ class Main extends Component {
   }
 
   handleBackButton () {
-    window.location.href = 'http://www.google.com'
+    window.location.href = window.history.back()
   }
 
   handleNotDone (taskId) {
@@ -110,7 +131,22 @@ class Main extends Component {
         <div className="flex-1 flex flex-col items-center justify-start bg-grey-lightest font-sans">
           <div className="flex-1 flex flex-col bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
             <div className="mb-4">
-              <h1 className="text-grey-darkest">Todo List</h1>
+              <div className="flex">
+                <h1 className="flex-1 text-grey-darkest">
+                  Todo List
+                </h1>
+                <button
+                  className="flex-no-shrink p-2 px-4 border-2 rounded text-black border-black hover:text-white hover:bg-black"
+                  onClick={this.handleReset}>
+                  <MlIcon
+                    icon="sync-alt"
+                    size="xs" />
+                  <span className="ml-4">Reset</span>
+                </button>
+              </div>
+              <p className="text-grey-darkest my-4">
+                This is a simple todo list example. There is some fake data generated with FakerJs. Feel free to add or remove new data. If you want to reset this initial configuration, with another random data, just press 'Reset' button. Enjoy!
+              </p>
 
               <AddItemsSection
                 onItemAdd={this.handleItemAdd}
